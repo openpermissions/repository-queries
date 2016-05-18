bcd50beac8c7c339451f8d0380b8f95c226b4f74
Bertrand Nouvel <bn@wide.io>
2016-05-18 15:48:49 +0100

    transfered changes in queries2

--- asset.py
+++ asset.py
@@ -6,11 +6,13 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
-# 
 
 
 ASSET_CLASS = "op:Asset"
 
+# Used in conjuncting with GENERIC_LIST query
+# NOTE: This return all possible combinations of entity_id and alsoIdentifiedBy_id
+# which is what we need for the index
 ASSET_LIST_ID_NAME = "entity"
 ASSET_LIST_EXTRA_IDS = """?source_id_value ?source_id_type"""
 ASSET_LIST_EXTRA_QUERY = """
@@ -20,25 +22,25 @@ ASSET_LIST_EXTRA_QUERY = """
 """
 
 ASSET_APPEND_ALSO_IDENTIFIED = """
-   {entity_id} op:alsoIdentifiedBy  _:bnode .
+   {entity_id} op:alsoIdentifiedBy  _:bnode{bnodeuuid0} .
   _:bnode{bnodeuuid0} op:id_type {source_id_type} .
   _:bnode{bnodeuuid0} op:value	{source_id_value} .
   _:bnode{bnodeuuid0} rdf:type	op:Id .
+    """
+
+ASSET_GET_ALSO_IDENTIFIED = """
+SELECT ?source_id_type ?source_id WHERE {{
+  {entity_id} op:alsoIdentifiedBy ?id .
+   ?id op:id_type  ?source_id_type .
+   ?id op:value ?source_id .
+}}
 """
 
-# CHANGE THE ASSOCIATED OFFER TO AN ASSET IF THE OFFER REFERS TO IT DIRECTLY
-ASSET_ADD_OFFER_DIRECT = '''
-INSERT
-{{
-   {new_offer_id} odrl:target ?s .
-}}
-WHERE
-{{
-  ?s rdf:type op:Asset .
-  {old_offer_id} odrl:target ?s .
-}}
-'''
 
+# Returns offers or agreements associated with an asset
+# The policies are associated either directly via odrl:target
+#  or via an a set and an asset selector through odrl:target/op:fromSet/op:hasElement
+# subquery : responsible for getting {idname}_entity {idname}_id_type {idname}_id_value
 ASSET_GET_POLICIES_FOR_ASSETS = """
 SELECT ?{idname}_id_bundle  (GROUP_CONCAT(DISTINCT ?entity_entity; separator='|') as ?ids) (GROUP_CONCAT(DISTINCT ?policy; separator='|') as ?policies)
 WHERE
@@ -58,21 +60,21 @@ GROUP BY ?{idname}_id_bundle
 """
 
 # UTILITY
-ASSET_QUERY_ALL_IDS = '''
+
+# NOTE: note used in blazegraph when onboarding
+# assets to identify the assets coming from the user.
+ASSET_QUERY_ALL_ENTITY_IDS = """
 PREFIX op: <http://openpermissions.org/ns/op/1.0/>
 
-SELECT ?entity_uri ?source_id_type ?source_id
-WHERE
+SELECT DISTINCT ?entity_id WHERE
 {
-  ?entity_uri rdf:type op:Asset ;
-           op:alsoIdentifiedBy ?altid .
-           ?altid op:id_type ?source_id_type ;
-                  op:value ?source_id .
+  ?entity_id rdf:type op:Asset  .
 }
-'''
+"""
 
+# NOTE: this is being used by ASSET_GET_POLICIES_FOR_ASSETS
 ASSET_SELECT_BY_ENTITY_ID = """
-SELECT ?{idname}_entity ?{idname}_id_value ?{idname}_id_type {{
+SELECT ?{idname}_entity ?{idname}_id_value ?{idname}_id_type WHERE {{
     VALUES (?{idname}_id_value) {{
         {idlist}
     }}
@@ -81,6 +83,7 @@ SELECT ?{idname}_entity ?{idname}_id_value ?{idname}_id_type {{
 }}
 """
 
+# NOTE: this is being used by ASSET_GET_POLICIES_FOR_ASSETS
 ASSET_SELECT_BY_SOURCE_ID = """
 SELECT ?{idname}_entity  ?{idname}_id_value ?{idname}_id_type {{
     VALUES ( ?{idname}_id_type ?{idname}_id_value) {{
@@ -92,5 +95,6 @@ SELECT ?{idname}_entity  ?{idname}_id_value ?{idname}_id_type {{
 }}
 """
 
-
+# Returns the subjects of the triples that are involved in an asset identified by asset_id
+# used by GENERIC_GET
 ASSET_STRUCT_SELECT = """ SELECT DISTINCT ?s {{ {id} (op:alsoIdentifiedBy)? ?s . }} """
